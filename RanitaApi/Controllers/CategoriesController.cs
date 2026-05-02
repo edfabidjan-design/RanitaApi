16125a495ed13b1aae6e80f66be9176a76b7b15b
edf abidjan <edfabidjan@yahoo.fr>
2026-05-02 15:11:41 +0000

    add categories

---
+++ RanitaApi/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RanitaApi.Data;
+using RanitaApi.Models;
+
+namespace RanitaApi.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Categories.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok(category);
+        }
+    }
+}
---
+++ RanitaApi/Models/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace RanitaApi.Models
+{
+    public class Category
+    {
+    }
+}
