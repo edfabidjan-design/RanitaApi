06fcb12f8dc064b79c6d505552e1442d433f8f48
edf abidjan <edfabidjan@yahoo.fr>
2026-05-02 15:53:14 +0000

    fix nullable category relation

--- RanitaApi/Models/Product.cs
+++ RanitaApi/Models/Product.cs
@@ -11,7 +11,7 @@
         public int Stock { get; set; }
 
         public string Description { get; set; } = "";
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
     }
 }
