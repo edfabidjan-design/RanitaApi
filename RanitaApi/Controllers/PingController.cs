acae593cd04948a581a187548e8aed4ab34d3330
edf abidjan <edfabidjan@yahoo.fr>
2026-05-02 12:29:51 +0000

    fix controllers

---
+++ RanitaApi/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RanitaApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Ranita API OK 🚀" });
+        }
+    }
+}
