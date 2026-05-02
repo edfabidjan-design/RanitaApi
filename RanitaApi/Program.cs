f16d7ed77c7440f8d64387944cf147aef34125f2
edf abidjan <edfabidjan@yahoo.fr>
2026-05-02 13:03:45 +0000

    remove ef usings

--- RanitaApi/Program.cs
+++ RanitaApi/Program.cs
@@ -1,12 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using RanitaApi.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
