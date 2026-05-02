853b634327585ea8b1257fe02867b86e3f715301
edf abidjan <edfabidjan@yahoo.fr>
2026-05-02 14:05:20 +0000

    connect postgres

--- RanitaApi/Program.cs
+++ RanitaApi/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using RanitaApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// 🔥 AJOUT IMPORTANT
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
