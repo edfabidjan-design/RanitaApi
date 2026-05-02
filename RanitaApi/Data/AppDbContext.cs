cea341f024864310cfd742e488323f9bbf98c50f
edf abidjan <edfabidjan@yahoo.fr>
2026-05-02 18:51:59 +0000

    fix product category relation

--- RanitaApi/Data/AppDbContext.cs
+++ RanitaApi/Data/AppDbContext.cs
@@ -12,5 +12,16 @@ namespace RanitaApi.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany()
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
     }
 }
+
