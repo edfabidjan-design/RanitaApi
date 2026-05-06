0fe66a89f04e24e6d2ad71c6e8fcf837993aaae5
edf abidjan <edfabidjan@yahoo.fr>
2026-05-06 22:16:48 +0000

    fix: use raw db connection for migrations

--- RanitaApi/Controllers/ProductsController.cs
+++ RanitaApi/Controllers/ProductsController.cs
@@ -258,22 +258,23 @@ namespace RanitaApi.Controllers
         {
             try
             {
-                var sql = new[]
-                {
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"OldPrice\" NUMERIC(18,2) NULL",
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"ShortDescription\" TEXT NOT NULL DEFAULT ''",
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"IsActive\" BOOLEAN NOT NULL DEFAULT TRUE",
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"Brand\" TEXT NOT NULL DEFAULT ''",
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"Slug\" TEXT NOT NULL DEFAULT ''",
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"MetaDescription\" TEXT NOT NULL DEFAULT ''",
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"Attributes\" TEXT NOT NULL DEFAULT '{}'",
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"Sku\" TEXT NOT NULL DEFAULT ''",
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"Images\" TEXT NOT NULL DEFAULT '[]'",
-            "ALTER TABLE \"Products\" ADD COLUMN IF NOT EXISTS \"ImageUrl\" TEXT NOT NULL DEFAULT ''"
-        };
-                foreach (var s in sql)
-                    _context.Database.ExecuteSqlRaw(s);
-
+                var conn = _context.Database.GetDbConnection();
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = @"
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""OldPrice"" NUMERIC(18,2) NULL;
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""ShortDescription"" TEXT NOT NULL DEFAULT '';
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""IsActive"" BOOLEAN NOT NULL DEFAULT TRUE;
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""Brand"" TEXT NOT NULL DEFAULT '';
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""Slug"" TEXT NOT NULL DEFAULT '';
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""MetaDescription"" TEXT NOT NULL DEFAULT '';
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""Attributes"" TEXT NOT NULL DEFAULT '{}';
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""Sku"" TEXT NOT NULL DEFAULT '';
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""Images"" TEXT NOT NULL DEFAULT '[]';
+            ALTER TABLE ""Products"" ADD COLUMN IF NOT EXISTS ""ImageUrl"" TEXT NOT NULL DEFAULT '';
+        ";
+                cmd.ExecuteNonQuery();
+                conn.Close();
                 return Ok("Migrations OK");
             }
             catch (Exception ex)
