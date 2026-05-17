b622d6217defc3e7f7728144eb4b8d5cfcc5aac3
Ranita Deploy <deploy@ranita.ci>
2026-05-17 17:50:16 +0000

    fix: desactiver Service Worker completement

--- RanitaApi/wwwroot/sw.js
+++ RanitaApi/wwwroot/sw.js
@@ -1,10 +1,6 @@
-// Service Worker désactivé - se supprime automatiquement
-self.addEventListener('install', () => self.skipWaiting());
+// SW désactivé
+self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-        .then(() => self.clients.claim())
-        .then(() => self.clients.matchAll().then(clients => clients.forEach(c => c.postMessage('reload'))))
-    );
+    e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))).then(() => self.clients.claim()));
 });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request.clone())); });
