36db8dfe084f9d8767c4e3cfeef5adc6ccf6bfc8
Ranita Deploy <deploy@ranita.ci>
2026-05-17 18:34:24 +0000

    fix: sw.js - toujours reseau jamais cache

--- RanitaApi/wwwroot/sw.js
+++ RanitaApi/wwwroot/sw.js
@@ -1,6 +1,15 @@
-// SW désactivé
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))).then(() => self.clients.claim()));
+// Service Worker désactivé complètement
+self.addEventListener('install', e => {
+    self.skipWaiting();
 });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request.clone())); });
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys()
+            .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+            .then(() => self.clients.claim())
+    );
+});
+self.addEventListener('fetch', e => {
+    // Toujours aller sur le réseau, jamais le cache
+    e.respondWith(fetch(e.request));
+});
